3c19bc35fe3fc3adb78a6cb5f440350d45333eff
btharding <btharding2014@hotmail.com>
2020-10-05 19:51:39 +0100

    Updates

---
+++ precache-manifest.a33fdbcaee2e5d49b0581a1d6889f8a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a785b873fc7a4d35c741ecd804539e",
+    "url": "/Veganalysis/index.html"
+  },
+  {
+    "revision": "843952ad7fc476b53028",
+    "url": "/Veganalysis/static/css/main.a538a9aa.chunk.css"
+  },
+  {
+    "revision": "95970d8b4621aae2a3fd",
+    "url": "/Veganalysis/static/js/2.e7a30702.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Veganalysis/static/js/2.e7a30702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843952ad7fc476b53028",
+    "url": "/Veganalysis/static/js/main.a0cf2e51.chunk.js"
+  },
+  {
+    "revision": "b6e1f11ba148ede77e29",
+    "url": "/Veganalysis/static/js/runtime-main.b096af10.js"
+  },
+  {
+    "revision": "997580836d556e12b9d6dbe49e2d724b",
+    "url": "/Veganalysis/static/media/icon.99758083.svg"
+  },
+  {
+    "revision": "ca519d31891d600bade45c88392a63aa",
+    "url": "/Veganalysis/static/media/logo.ca519d31.svg"
+  }
+]);
